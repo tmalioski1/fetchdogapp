983920bc2c563a81c999a9eefb803dcfee3d9990
Tyler Malinoski <tmalinoski1@gmail.com>
2023-10-11 22:35:11 -0700

    nav-bar-conditionally-renders

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Homepage from './components/Homepage';
 import DogSearch from './components/DogSearch';
+import { useSelector } from "react-redux";
+
 
 function App() {
+  const isAuthenticated = useSelector(state => state.session?.user !== null);
+
   return (
     <>
-      <Navbar />
+      {isAuthenticated && <Navbar />}
       <Routes>
         <Route path="/search" element={<DogSearch />} />
         <Route path="/" element={<Homepage />} />
